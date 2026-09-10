0bec32f268dd7e61615df12fe3fa67156cce6f26
Trevor Micek <webdevtrevor@gmail.com>
2026-09-10 16:45:01 -0600

    updated copy

--- src/components/Layout/Hero.jsx
+++ src/components/Layout/Hero.jsx
@@ -118,10 +118,12 @@ export default function HeroComp() {
               <div className=" max-w-md px-2 sm:max-w-2xl sm:px-9  lg:px-0 lg:text-left lg:flex lg:items-center">
                 <div className="">
                   <h1 className="mt-8 tiny:text-[1.9rem] xs:text-4xl text-3.5xl font-mont font-[425]  text-white sm:mt-5 sm:text-5xl .5xl:text-5.5xl .5xl:py-1 lg:mt-6 xl:text-6xl">
-                    <span className="block">
-                      Music, Art, and Community. Built by Denver, for Denver.
-                    </span>
+                    Welcome to YartistFest
                   </h1>
+                  <p className="mt-3 text-gray-300 sm:mt-5 sm:text-xl .5xl:pt-4 .5xl:-mb-4 .5xl:text-1.5xl xl:text-xl min-w-full ">
+                    Great Music. Amazing Food. A Brighter Future for Denver
+                    Students.
+                  </p>
                   <FadeIn>
                     <div className="mt-10 sm:mt-12">
                       <div className="sm:col-span-2">
@@ -130,7 +132,7 @@ export default function HeroComp() {
                             href="/about"
                             className=" inline-flex items-center justify-center px-20 tiny:px-16  xs:px-24 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-900 hover:bg-gray-800 .5xl:text-lg .5xl:px-7"
                           >
-                            Explore the Festival
+                            Our Mission
                           </a>
                         </div>
                       </div>
